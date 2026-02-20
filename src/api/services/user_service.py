fb6c3869139e2770b1d912a25baab4d0eaf79381
HSalasPedr <119406196+HSalasPedr@users.noreply.github.com>
2026-02-20 18:10:05 +0000

    minor changes

--- src/api/services/user_service.py
+++ src/api/services/user_service.py
@@ -3,7 +3,7 @@ Servicio de usuarios - Logica de negocio para CRUD de User
 """
 
 from flask import abort
-from api.models import db, User, ProfileInfo
+from api.models import db, User
 
 
 class UserService:
