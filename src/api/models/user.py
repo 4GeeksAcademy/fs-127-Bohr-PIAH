c263dc7e462500d9865ab36eb439b138d2c312d9
HSalasPedr <119406196+HSalasPedr@users.noreply.github.com>
2026-03-02 18:07:14 +0000

    user model updated

--- src/api/models/user.py
+++ src/api/models/user.py
@@ -32,13 +32,13 @@ class User(db.Model):
     last_name: Mapped[str] = mapped_column(String(100))
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
     password: Mapped[str] = mapped_column(String(255))
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True)#añadida
-    role: Mapped[RoleName] = mapped_column(Enum(RoleName), nullable=False, default=RoleName.guest)#cambiado
-    
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)  # añadida
+    role: Mapped[RoleName] = mapped_column(
+        Enum(RoleName), nullable=False, default=RoleName.guest)  # cambiado
 
-    #Relaciones
+    # Relaciones
     department: Mapped["Department"] = relationship(
-        "department",
+        "Department",
         back_populates="user",
         cascade="all, delete-orphan",
     )
