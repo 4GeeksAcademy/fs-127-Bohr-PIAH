f694722436b4daa8c1acde71af685507397c1b35
HSalasPedr <119406196+HSalasPedr@users.noreply.github.com>
2026-03-02 19:01:11 +0000

    user model small fix

--- src/api/models/user.py
+++ src/api/models/user.py
@@ -95,5 +95,5 @@ class User(db.Model):
             "email": self.email,
             "first_name": self.first_name,
             "last_name": self.last_name,
-            "role": self.role
+            "role": self.role.value
         }
