8b2161c5f0473d71742a9c800b54534d7d0fc5fd
Paty81 <molinamartosp@gmail.com>
2026-03-20 16:17:33 +0000

    socorro a ver si sale

--- src/front/store.js
+++ src/front/store.js
@@ -7,6 +7,7 @@ export const initialStore = () => {
     departments: [],
     currentProjectId: null,
     tasks: [],
+    users: [],
     todos: [
       {
         id: 1,
@@ -95,6 +96,12 @@ export default function storeReducer(store, action = {}) {
         ...store,
         departments: action.payload,
       };
+
+    case "set_users":
+    return {
+        ...store,
+        users: action.payload,
+    };
 
     case "set_token":
       localStorage.setItem("token", action.payload);
